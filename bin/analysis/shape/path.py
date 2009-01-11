0bafbe2a241c65c5ecbfae48db9d0f51f39d1c7a
Nick Bray <ncbray@gmail.com>
2009-01-11 12:31:51 -0600

    Shape Analysis: Added path equivilence class.

---
+++ bin/analysis/shape/path.py
@@ -0,0 +1,283 @@
+# Set intersection
+# Set intersection test
+# Set union
+
+# Path in set test
+# Path kill
+# Path point to kill
+# Path union
+
+##from PADS.UnionFind import UnionFind
+
+
+class UnionFind(object):
+	__slots__ = 'parents', 'weights'
+
+	def __init__(self):
+		self.parents = {}
+		self.weights = {}
+
+	def __getitem__(self, obj):
+		if obj not in self.parents:
+			return obj
+		else:
+			return self.getItemCompress(obj)
+
+	def getItemCompress(self, obj):
+		parent = self.parents[obj]
+		if parent == obj:
+			return parent
+		else:
+			root = self.getItemCompress(parent)
+			self.parents[obj] = root
+			return root
+
+	def union(self, first, *objs):
+		if objs:
+			biggestRoot = self[first]
+			maxWeight   = self.weights.get(biggestRoot, 1)
+			roots       = set()
+			roots.add(biggestRoot)
+
+			for obj in objs:
+				root   = self[obj]
+				if root not in roots:
+					weight = self.weights.get(root, 1)
+					if weight > maxWeight:
+						biggestRoot = root
+						maxWeight   = weight
+					roots.add(root)
+
+			# The biggest root is intentionall left in roots,
+			# So we ensure that self.parents[biggestRoot] exists.
+			for root in roots:
+				self.parents[root] = biggestRoot
+				weight += self.weights.pop(root, 1)
+
+			self.weights[biggestRoot] = weight
+
+def length(path):
+	tail, head = path
+	if tail is None:
+		return 1
+	else:
+		return length(tail)+1
+	
+##class PathEquivilence(object):
+##	def __init__(self):
+##		self._canonical = {}
+##
+##	def _canonicalCompress(self, path):
+##		parent = self._canonical.get(path, path)
+##
+##		if path == parent:
+##			return path
+##		else:
+##			root = self.canonical(parent)
+##			self._canonical[path] = root
+##			return root
+##
+##	def _canonicalTail(self, path):
+##		tail, head = path
+##		
+##		newtail = None if tail is None else self.canonical(tail)
+##		if tail != newtail:
+##			newpath = (newtail, head)
+##		else:
+##			newpath = path
+##		return newpath
+##	
+##	def canonical(self, path):
+##		newpath = self._canonicalTail(path)			
+##		compressed = self._canonicalCompress(newpath)
+##		return compressed
+##
+##	def union(self, *paths):
+##		cpaths = [self.canonical(path) for path in paths]
+##		cpathsSet = set(cpaths)
+##		
+##		if len(cpathsSet) > 1:
+##			cfirst         = cpaths[0]
+##			shortestPath   = cfirst
+##			shortestLength = length(cfirst)
+##
+##			for cpath in cpathsSet:
+##				l = length(cpath)
+##				if  l < shortestLength:
+##					shortestPath = cpath
+##					shortestLength = l
+##
+##			#cpathsSet.remove(shortestPath) #?
+##			for cpath in cpathsSet:
+##				self._canonical[cpath] = shortestPath
+##
+##			self.compress()
+##
+##
+##	def compress(self):
+##		def chain(d, key, value):
+##			if key != value:
+##				# key -> value
+##				
+##				if key in d:
+##					other = d[key]
+##					if length(value) >= length(other):
+##						# key -> other  and  value -> other
+##						chain(d, value, other)
+##					else:
+##						# key -> value  and  other -> value
+##						d[key] = value
+##						chain(d, other, value)
+##				else:			
+##					d[key] = value
+##
+##		outp = {}		
+##		for k, v in self._canonical.iteritems():
+##			ck = self._canonicalTail(k)
+##			cv = self.canonical(v)
+##			chain(outp, ck, cv)
+##
+##		self._canonical = outp
+##
+##
+##	def dump(self):
+##		for k, v in self._canonical.iteritems():
+##			print "%r  ->  %r" % (k, v)
+##
+##
+##	def intersection(self, other):
+##		pairC = {}
+##		outp  = {}
+##
+##		p = PathEquivilence()
+##
+##		def addValue(k):
+##			c1 = self.canonical(k)
+##			c2 = other.canonical(k)
+##			pair = (c1, c2)
+##			
+##			if pair not in pairC:
+##				pairC[pair] = k
+##			else:
+##				p.union(k, pairC[pair])
+##
+##		for k, v in self._canonical.iteritems():
+##			addValue(k)
+##			addValue(v)
+##
+##		p._canonical.update(outp)
+##		return p
+
+
+
+class PathEquivilence(object):
+	def __init__(self):
+		self._canonical = {}
+
+	def _canonicalCompress(self, path):
+		parent = self._canonical.get(path, path)
+
+		if path == parent:
+			return path
+		else:
+			root = self.canonical(parent)
+			self._canonical[path] = root
+			return root
+
+	def _canonicalTail(self, path):
+		tail, head = path
+		
+		newtail = None if tail is None else self.canonical(tail)
+		if tail != newtail:
+			newpath = (newtail, head)
+		else:
+			newpath = path
+		return newpath
+	
+	def canonical(self, path):
+		newpath = self._canonicalTail(path)			
+		compressed = self._canonicalCompress(newpath)
+		return compressed
+
+	def union(self, *paths):
+		cpaths = [self.canonical(path) for path in paths]
+		cpathsSet = set(cpaths)
+		
+		if len(cpathsSet) > 1:
+			cfirst         = cpaths[0]
+			shortestPath   = cfirst
+			shortestLength = length(cfirst)
+
+			for cpath in cpathsSet:
+				l = length(cpath)
+				if  l < shortestLength:
+					shortestPath = cpath
+					shortestLength = l
+
+			#cpathsSet.remove(shortestPath) #?
+			for cpath in cpathsSet:
+				self._canonical[cpath] = shortestPath
+
+			self.compress()
+
+
+	def compress(self):
+		def chain(d, key, value):
+			if key != value:
+				# key -> value
+				
+				if key in d:
+					other = d[key]
+					if length(value) >= length(other):
+						# key -> other  and  value -> other
+						chain(d, value, other)
+					else:
+						# key -> value  and  other -> value
+						d[key] = value
+						chain(d, other, value)
+				else:			
+					d[key] = value
+
+		outp = {}		
+		for k, v in self._canonical.iteritems():
+			ck = self._canonicalTail(k)
+			cv = self.canonical(v)
+			chain(outp, ck, cv)
+
+		self._canonical = outp
+
+
+	def dump(self):
+		for k, v in self._canonical.iteritems():
+			print "%r  ->  %r" % (k, v)
+
+
+	def setIntersection(self, other):
+		pairC = {}
+
+		p = PathEquivilence()
+
+		def addValue(k):
+			c1 = self.canonical(k)
+			c2 = other.canonical(k)
+			pair = (c1, c2)
+			
+			if pair not in pairC:
+				pairC[pair] = k
+			else:
+				p.union(k, pairC[pair])
+
+		for k, v in self._canonical.iteritems():
+			addValue(k)
+			addValue(v)
+
+		p._canonical.update(outp)
+		return p
+
+	def setUnion(self, other):
+		p = PathEquivilence()
+		p._canonical.update(self._canonical)
+
+		for k, v in other._canonical.iteritems():
+			p.union(k, v)
+		return p
--- bin/tests/shape/test_shape_paths.py
+++ bin/tests/shape/test_shape_paths.py
@@ -1,3 +1,251 @@
 from __future__ import absolute_import
 
 import unittest
+
+import analysis.shape.path as path
+
+
+class TestUnionFind(unittest.TestCase):
+
+	def testUnion(self):
+		u = path.UnionFind()
+
+		u.union(1, 2, 3)
+		u.union(4, 5, 6)
+
+		# Set 1
+		self.assertEquals(u[1], u[2])
+		self.assertEquals(u[1], u[3])
+
+		# Set 2
+		self.assertEquals(u[4], u[5])
+		self.assertEquals(u[4], u[6])
+
+		# Set != Set
+		self.assertNotEquals(u[1], u[4])
+
+		# Set != untracked
+		self.assertNotEquals(u[1], u[7])
+
+		# Make sure we're clean.
+		self.assertEquals(len(u.parents), 6)
+		self.assertEquals(len(u.weights), 2)
+
+
+class TestPathEquivilence(unittest.TestCase):
+	def setUp(self):
+		self.pe = path.PathEquivilence()
+		
+		self.x = (None, 'x')
+		self.xn = (self.x, 'n')
+		self.xnn = (self.xn, 'n')
+
+		self.y = (None, 'y')
+		self.yn = (self.y, 'n')
+		self.ynn = (self.yn, 'n')
+
+		self.z = (None, 'z')
+		self.zn = (self.z, 'n')
+		self.znn = (self.zn, 'n')
+
+		self.w = (None, 'w')
+		self.wn = (self.w, 'n')
+		self.wnn = (self.wn, 'n')
+
+	def dump(self):
+		print
+		print "DUMP"
+		self.pe.dump()
+		print
+
+	def testUnion1(self):
+		self.pe.union(self.x, self.y)
+		self.assertEqual(self.pe.canonical(self.x),   self.pe.canonical(self.y))
+		self.assertEqual(self.pe.canonical(self.xn),  self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.xnn), self.pe.canonical(self.ynn))
+
+
+	def testUnion2(self):
+		self.pe.union(self.xn, self.yn)
+		self.assertNotEqual(self.pe.canonical(self.x),   self.pe.canonical(self.y))
+		self.assertEqual(self.pe.canonical(self.xn),     self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.xnn),    self.pe.canonical(self.ynn))
+
+	def testUnion3(self):
+		self.pe.union(self.xnn, self.ynn)
+		self.assertNotEqual(self.pe.canonical(self.x),   self.pe.canonical(self.y))
+		self.assertNotEqual(self.pe.canonical(self.xn),  self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.xnn),    self.pe.canonical(self.ynn))
+
+	def testRecursive(self):
+		self.pe.union(self.x, self.xn)
+		self.assertEqual(self.pe.canonical(self.x),   self.pe.canonical(self.xn))
+		self.assertEqual(self.pe.canonical(self.x),   self.pe.canonical(self.xnn))
+
+
+	def testTricky1_0(self):
+		self.pe.union(self.xn, self.yn)
+		self.pe.union(self.x, self.z)
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.xn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.yn))
+
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.ynn))
+
+	def testTricky1_1(self):
+		self.pe.union(self.xn, self.yn)
+		self.pe.union(self.z, self.x)
+
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.xn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.yn))
+
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.ynn))
+
+	def testTricky1_2(self):
+		self.pe.union(self.yn, self.xn)
+		self.pe.union(self.x, self.z)
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.xn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.yn))
+
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.ynn))
+
+	def testTricky1_3(self):
+		self.pe.union(self.yn, self.xn)
+		self.pe.union(self.z, self.x)
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.xn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.yn))
+
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.ynn))
+
+
+	def testTricky2(self):
+		self.pe.union(self.xnn, self.ynn)
+		self.pe.union(self.z, self.x)
+
+		self.assertNotEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.xn))
+		self.assertNotEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.yn))
+
+		self.assertEqual(self.pe.canonical(self.xnn),   self.pe.canonical(self.ynn))
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.xnn))
+		self.assertEqual(self.pe.canonical(self.znn),   self.pe.canonical(self.ynn))
+
+
+	def testTricky3_0(self):
+		self.pe.union(self.xn, self.yn)
+		self.pe.union(self.wn, self.zn)
+		self.pe.union(self.z, self.x)
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.wn))
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.zn))
+
+		self.assertNotEqual(self.pe.canonical(self.x),   self.pe.canonical(self.y))
+		self.assertNotEqual(self.pe.canonical(self.z),   self.pe.canonical(self.w))
+		self.assertEqual(self.pe.canonical(self.x),   self.pe.canonical(self.z))
+
+
+	def testTricky3_1(self):
+		self.pe.union(self.yn, self.xn)
+		self.pe.union(self.wn, self.zn)
+		self.pe.union(self.z, self.x)
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.yn))
+		self.assertEqual(self.pe.canonical(self.zn),   self.pe.canonical(self.wn))
+
+		self.assertEqual(self.pe.canonical(self.xn),   self.pe.canonical(self.zn))
+
+		self.assertNotEqual(self.pe.canonical(self.x),   self.pe.canonical(self.y))
+		self.assertNotEqual(self.pe.canonical(self.z),   self.pe.canonical(self.w))
+		self.assertEqual(self.pe.canonical(self.x),   self.pe.canonical(self.z))
+
+
+
+class TestPathEquivilenceOps(unittest.TestCase):
+	def setUp(self):		
+		self.x = (None, 'x')
+		self.xn = (self.x, 'n')
+		self.xnn = (self.xn, 'n')
+
+		self.y = (None, 'y')
+		self.yn = (self.y, 'n')
+		self.ynn = (self.yn, 'n')
+
+		self.z = (None, 'z')
+		self.zn = (self.z, 'n')
+		self.znn = (self.zn, 'n')
+
+		self.a = (None, 'a')
+		self.an = (self.a, 'n')
+		self.ann = (self.an, 'n')
+
+		self.b = (None, 'b')
+		self.bn = (self.b, 'n')
+		self.bnn = (self.bn, 'n')
+
+		self.c = (None, 'c')
+		self.cn = (self.c, 'n')
+		self.cnn = (self.cn, 'n')
+
+
+	def dump(self):
+		print
+		print "DUMP"
+		self.pe.dump()
+		print
+
+
+	def testIntersection(self):
+		pe1 = path.PathEquivilence()
+		pe1.union(self.a, self.b, self.c)
+		pe1.union(self.x, self.y, self.z)
+
+		pe2 = path.PathEquivilence()
+		pe2.union(self.a, self.b, self.z)
+		pe2.union(self.x, self.y, self.c)
+
+		pe3 = pe1.setIntersection(pe2)
+
+		self.assertEqual(pe3.canonical(self.a),      pe3.canonical(self.b))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.c))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.x))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.y))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.z))
+
+		self.assertEqual(pe3.canonical(self.x),      pe3.canonical(self.y))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.z))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.a))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.b))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.c))
+		
+
+	def testIntersection(self):
+		pe1 = path.PathEquivilence()
+		pe1.union(self.a, self.b)
+		pe1.union(self.x, self.y)
+
+		pe2 = path.PathEquivilence()
+		pe2.union(self.a, self.c)
+		pe2.union(self.x, self.z)
+
+		pe3 = pe1.setUnion(pe2)
+
+		self.assertEqual(pe3.canonical(self.a),      pe3.canonical(self.b))
+		self.assertEqual(pe3.canonical(self.a),      pe3.canonical(self.c))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.x))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.y))
+		self.assertNotEqual(pe3.canonical(self.a),   pe3.canonical(self.z))
+
+		self.assertEqual(pe3.canonical(self.x),      pe3.canonical(self.y))
+		self.assertEqual(pe3.canonical(self.x),      pe3.canonical(self.z))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.a))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.b))
+		self.assertNotEqual(pe3.canonical(self.x),   pe3.canonical(self.c))

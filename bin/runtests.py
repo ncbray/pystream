d7e6ae92bcc6780e297ffeae13bcf8bd81337742
Nick Bray <ncbray@gmail.com>
2009-01-20 15:02:40 -0600

    Added guard to require Python 2.6.

--- bin/runtests.py
+++ bin/runtests.py
@@ -1,6 +1,12 @@
 #!/c/python25/python
 from __future__ import absolute_import
 
+import sys
+major, minor = sys.version_info[:2]
+if major != 2 or minor < 6:
+	print "PyStream requires Python 2.6 to run.  Python %d.%d detected." % (major, minor)
+	sys.exit()
+
 import scriptsetup
 import config
 import testspider

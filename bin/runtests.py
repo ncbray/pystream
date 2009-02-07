a258cb6c6982e84f18a289039bf5c6523a4830d5
Nick Bray <ncbray@gmail.com>
2009-02-07 14:15:51 -0600

    BUGFIX: eliminated windows-spesific library path.

--- bin/runtests.py
+++ bin/runtests.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 
 import sys
+import os
 major, minor = sys.version_info[:2]
 if major != 2 or minor < 6:
 	print "PyStream requires Python 2.6 to run.  Python %d.%d detected." % (major, minor)
@@ -12,11 +13,11 @@ import config
 import testspider
 
 root = scriptsetup.scriptRoot(__file__)
-scriptsetup.libraryDirectory(root, "../lib")
+scriptsetup.libraryDirectory(root, os.path.join("..", "lib"))
 if config.usePsyco: scriptsetup.initPsyco()
 
 testList    = getattr(config, 'limitedTest', None)
 testExclude = getattr(config, 'testExclude', ())
 
 
-testspider.runTests(root, testList, testExclude)	
+testspider.runTests(root, testList, testExclude)

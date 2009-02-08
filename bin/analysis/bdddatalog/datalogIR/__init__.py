1d57136bbfc6f4ec01bc5b184e1591487b40ec37
Nick Bray <ncbray@gmail.com>
2009-02-08 10:48:56 -0600

    Removed deprecated modules related to BDD analysis.

--- bin/analysis/bdddatalog/datalogIR/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from . import parser
-from . import optimizer
-
-def parseAlgorithim(algorithim):
-	ast = parser.astFromAlgorithim(algorithim)
-	ast = optimizer.simplifyAST(ast)
-	return ast

58f889c6a8238b3fb4df8eb7168b46722fb8c5e4
Nick Bray <ncbray@gmail.com>
2009-04-09 13:51:40 -0500

    Metaclass for typedispatcher now works correctly when inherited from.

--- bin/util/typedispatch.py
+++ bin/util/typedispatch.py
@@ -1,7 +1,11 @@
-__all__ = ['typedispatcher', 'defaultdispatch', 'dispatch',
+__all__ = ['typedispatcher', 'TypeDispatcher', 'StrictTypeDispatcher',
+           'defaultdispatch', 'dispatch',
            'allChildren', 'visitAllChildren']
 
+# Not used inside this module, only exported.
 from xform import allChildren, visitAllChildren
+
+import inspect
 
 def dispatch(*types):
 	def dispatchF(f):
@@ -17,47 +21,72 @@ def defaultdispatch(f):
 	def defaultWrap(*args, **kargs):
 		return f(*args, **kargs)
 	defaultWrap.__original__ = f
-	defaultWrap.__dispatch__ = 'default'
+	defaultWrap.__dispatch__ = ('default',)
 	return defaultWrap
 
 def dispatch__call__(self, p, *args):
 	t = type(p)
+	table = self.__typeDispatchTable__
 
-	if not t in self.__dispatchTable__:
-		# Binds method.
-		return self.__dispatchDefault__(p, *args)
-	else:
-		# Does not bind method.
-		return self.__dispatchTable__[t](self, p, *args)
+	if not t in table: t = 'default'
+
+	return table[t](self, p, *args)
 
 
-def null(self, p, *args):
-	return p
+class TypeDispatchError(Exception):
+	pass
 
-def typedispatcher(name, bases, d):
+def exceptionDefault(self, node, *args):
+	raise TypeDispatchError, "%r cannot handle %r" % (type(self), type(node))
 
-	lut = {}
-	restore = {}
 
-	default = null
+def nullDefault(self, node, *args):
+	return node
 
-	for k, v in d.iteritems():
-		if hasattr(v, '__dispatch__') and hasattr(v, '__original__'):
-			types = v.__dispatch__
-			original = v.__original__
 
-			if types == 'default':
-				default = original
-			else:
+def inlineAncestor(t, lut):
+	if hasattr(t, '__typeDispatchTable__'):
+		# Search for types that haven't been defined, yet.
+		for k, v in t.__typeDispatchTable__.iteritems():
+			if k not in lut:
+				lut[k] = v
+
+class typedispatcher(type):
+	def __new__(mcls, name, bases, d):
+		lut = {}
+		restore = {}
+
+		# Build the type lookup table from the local declaration
+		for k, v in d.iteritems():
+			if hasattr(v, '__dispatch__') and hasattr(v, '__original__'):
+				types = v.__dispatch__
+				original = v.__original__
+
 				for t in types:
 					assert not t in lut, (name, t)
 					lut[t] = original
-			restore[k] = original
 
-	d.update(restore)
+				restore[k] = original
 
-	d['__dispatchTable__'] = lut
-	d['__dispatchDefault__'] = default
-	d['__call__'] = dispatch__call__
+		# Remove the wrappers from the methods
+		d.update(restore)
 
-	return type(name, bases, d)
+		# Search and inline dispatch tables from the MRO
+		for base in bases:
+			for t in inspect.getmro(base):
+				inlineAncestor(t, lut)
+
+		if 'default' not in lut:
+			lut['default'] = nullDefault
+
+		d['__typeDispatchTable__'] = lut
+		d['__call__'] = dispatch__call__
+
+		return type.__new__(mcls, name, bases, d)
+
+class TypeDispatcher(object):
+	__metaclass__ = typedispatcher
+	nullDefault = defaultdispatch(nullDefault)
+
+class StrictTypeDispatcher(TypeDispatcher):
+	exceptionDefault = defaultdispatch(exceptionDefault)

d451aee7089bd8612fd50d95dfd8a03b4813c132
Nick Bray <ncbray@gmail.com>
2009-12-12 11:17:04 -0600

    Improved origin strings

--- bin/asttools/origin.py
+++ bin/asttools/origin.py
@@ -1,13 +1,33 @@
 from util.xcollections import namedtuple
 
 def originString(origin):
-	if origin is None:
-		return "<unknown origin>"
-	elif origin.lineno is None or origin.lineno < 0:
-		return "File \"%s\" in %s" % (origin.filename, origin.name)
+	if origin is None: return "<unknown origin>"
+
+        if origin.filename:
+                s = "File \"%s\"" % origin.filename
+        else:
+                s = ''
+
+	
+	if origin.lineno is None or origin.lineno < 0:
+                needComma = False
 	elif origin.col is None or origin.col < 0:
-		return "File \"%s\", line %d, in %s" % (origin.filename, origin.lineno, origin.name)
+                if s: s += ', '
+		s = "%sline %d" % (s, origin.lineno)
+                needComma = True
 	else:
-		return "File \"%s\", line %d:%d, in %s" % (origin.filename, origin.lineno, origin.col, origin.name)
+                if s: s += ', '
+		s = "%sline %d:%d" % (s, origin.lineno, origin.col)
+                needComma = True
+
+        if origin.name:
+                if s:
+                        if needComma:
+                                s += ', '
+                        else:
+                                s += ' '
+                s = "%sin %s" % (s, origin.name)
+
+        return s
 
 Origin = namedtuple('Origin', 'name filename lineno col', dict(originString=originString))
